cfdcec77f6f8ff11bbc20b986cf77dc9860f8fe6
kadubarral <kadubarral@gmail.com>
2021-03-31 02:16:56 +0100

    fix app start

--- app.py
+++ app.py
@@ -1,24 +1,16 @@
 import os
 
-from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager
-
 from app import blueprint
-from app.main import create_app, db
+from app.main import create_app
 from app.main.model import user
 from dotenv import load_dotenv
 
 load_dotenv()
-
 
 app = create_app(os.getenv('APP_SETTINGS'))
 app.register_blueprint(blueprint)
 
 app.app_context().push()
 
-manager = Manager(app)
-migrate = Migrate(app, db)
-manager.add_command('db', MigrateCommand)
-
 if __name__ == '__main__':
-    manager.run()
+    app.run()
